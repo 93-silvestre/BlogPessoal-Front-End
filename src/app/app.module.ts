0a1bd6f54c179b639bf35135a7fb6e069f52e8cf
93-silvestre <nathyribsilvestre@gmail.com>
2021-08-31 13:55:10 -0300

    Componente inicio sem consumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -31,7 +32,11 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
